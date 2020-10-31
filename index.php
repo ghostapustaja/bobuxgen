713096e89829ab5899eb61e3a19a8c9dd896d319
ghostapustaja <67878210+ghostapustaja@users.noreply.github.com>
2020-10-31 20:50:39 +0200

    dont skid or gay
    yes

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Bobux Generator</title> 
+<body bgcolor=black> 
+<center> 
+<h3><font color="FFFFFF" size="10" face="Calibri">
+<p><b>bobux gen</b></p>
+<a href="https://www.youtube.com/watch?v=DLzxrzFCyOs">GET FREE BOBUX</a></p>
+<img src="https://thumbs.gfycat.com/DismalNaiveGossamerwingedbutterfly.webp" alt="Bobux">
+<p><b>NO CLICKBAIT.</b></p>
+<a href="https://github.com/ghostapustaja/bobuxgen">GitHub Project Page</a></p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ACcpVNJTd4Q?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <footer>&copy; 2020 ghostapustaja</footer>
